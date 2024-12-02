b56ed4a594f3efde7c3da5ee5e2c622c1b5c8787
John Barksdale <john.barksdale@leggett.com>
2024-12-02 11:29:03 -0600

    Second Star Earened

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# 2024_advent_of_code" 
+# 2024 Advent of Code 
+
+## Stars Achieved
+- Day 1.1
+- Day 1.2
---
+++ libs/day1_2.py
@@ -0,0 +1,26 @@
+from collections import Counter
+import pathlib as pl
+
+from libs.day1_1 import generate_two_lists
+
+if __name__ == '__main__':
+    input_data_file = pl.Path(r'c:\projects\adventofcode\data\day_1_1\input.txt')
+    input_data_list = []
+
+    with open(input_data_file, 'r') as f:
+        for line in f:
+            input_data_list.append(line)
+
+    list_1, list_2 = generate_two_lists(input_data_list)
+
+    dict_of_repeated_elements = Counter(list_2)
+
+    similarity_list = []
+
+    for value in list_1:
+        if value in dict_of_repeated_elements.keys():
+            similarity_list.append(value * dict_of_repeated_elements[value])
+
+    similarity_sum = sum(similarity_list)
+
+    print(similarity_sum)
