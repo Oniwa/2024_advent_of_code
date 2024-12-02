dd1fc2439509661f08697d96fc7768d5c9cd8822
John Barksdale <john.barksdale@leggett.com>
2024-12-02 09:53:25 -0600

    Tests pass

--- libs/day1_1.py
+++ libs/day1_1.py
@@ -1,2 +1,10 @@
-def generate_two_lists(input_list: list[str]) -> tuple[list[str], list[str]]:
-    pass
+def generate_two_lists(input_list: list[str]) -> tuple[list[int], list[int]]:
+    list1 = []
+    list2 = []
+
+    for item in input_list:
+        list1.append(int(item[0].split(' ')[0]))
+        list2.append(int(item[0].split(' ')[1]))
+
+    return list1, list2
+
