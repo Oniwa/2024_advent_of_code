d4e972b965b23a7a9ca127f0bdd88be287af488c
John Barksdale <john.barksdale@leggett.com>
2024-12-02 10:29:23 -0600

    Modified test generate two lists to have 3 spaces between ints instead of 1

--- libs/day1_1.py
+++ libs/day1_1.py
@@ -36,6 +36,12 @@ if __name__ == '__main__':
     sorted_list_1 = sorted(list_1)
     sorted_list_2 = sorted(list_2)
 
+    combined_lists = combine_lists(sorted_list_1, sorted_list_2)
+
     diff_list = []
+    for combined_item in combined_lists:
+        diff_list.append(abs(combined_item[1] - combined_item[0]))
 
+    sum_of_list = sum(diff_list)
 
+    print(sum_of_list)
