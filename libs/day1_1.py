4e45e373c786cba102f2e537d5766fb97e4d290f
John Barksdale <john.barksdale@leggett.com>
2024-12-02 10:02:12 -0600

    Modified test and function to match file input

--- libs/day1_1.py
+++ libs/day1_1.py
@@ -1,10 +1,22 @@
+import pathlib as pl
+
+
 def generate_two_lists(input_list: list[str]) -> tuple[list[int], list[int]]:
     list1 = []
     list2 = []
 
     for item in input_list:
-        list1.append(int(item[0].split(' ')[0]))
-        list2.append(int(item[0].split(' ')[1]))
+        list1.append(int(item.split(' ')[0]))
+        list2.append(int(item.split(' ')[1]))
 
     return list1, list2
 
+if __name__ == '__main__':
+    input_data_file = pl.Path(r'c:\projects\adventofcode\data\day_1_1\input.txt')
+    input_data_list = []
+
+    with open(input_data_file, 'r') as f:
+        for line in f:
+            input_data_list.append(line)
+
+    pass
--- test/test_day_1_1.py
+++ test/test_day_1_1.py
@@ -1,7 +1,7 @@
 import libs.day1_1 as day1_1
 
 def test_generate_two_lists():
-    input_list = [['1 10'], ['2 20'], ['5 50'], ['3 30'], ['4 40'], ['6 60']]
+    input_list = ['1 10\n', '2 20\n', '5 50\n', '3 30\n', '4 40\n', '6 60\n']
 
     list1, list2 = day1_1.generate_two_lists(input_list)
 
