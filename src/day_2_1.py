d459596d6d6cb4559ab7ad664af0f772faddf8b0
jdbarks <jdbarks@gmail.com>
2024-12-02 23:31:14 -0600

    Refactored day 2 code to be more efficient and added comments

--- src/day_2_1.py
+++ src/day_2_1.py
@@ -3,20 +3,22 @@ import pathlib as pl
 import src.libs.advent_helper as ah
 
 if __name__ == "__main__":
+    # Get list of reports
     report_list = ah.read_file_to_list(pl.Path('../data/day_2_1/input.txt'))
 
+    # Find number of safe reports with and without dampener
     safe_reports = 0
+    safe_reports_with_dampener = 0
 
     for report in report_list:
+        # Check to see if report is safe without dampener
         if ah.is_report_safe(report):
             safe_reports += 1
+            safe_reports_with_dampener += 1
+        else:
+            # Check to see if report is safe with dampener
+            if ah.is_report_safe_with_dampener(report):
+                safe_reports_with_dampener += 1
 
     print(f'Safe reports without dampener: {safe_reports}')
-
-    safe_reports = 0
-
-    for report in report_list:
-        if ah.is_report_safe_with_dampener(report):
-            safe_reports += 1
-
-    print(f'Safe reports with dampener: {safe_reports}')
+    print(f'Safe reports with dampener: {safe_reports_with_dampener}')
