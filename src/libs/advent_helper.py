02bbd17df97a77060f1a9adf9005928c2fe015d6
John Barksdale <jdbarks@gmail.com>
2024-12-04 14:15:40 -0600

    Created find_xmas_function and all tests pass

--- src/libs/advent_helper.py
+++ src/libs/advent_helper.py
@@ -162,3 +162,21 @@ def evaluate_instruction(instruction:str) -> int:
     result = func_to_call(int(args[0]), int(args[1]))
 
     return result
+
+
+def find_xmas_count(search_string: str) -> int:
+    """
+    Finds all matchs of XMAS or SAMX (XMAS in reverse) in a search string.  The match can be part of a different match.
+    IE: SAMXMAS matches both XMAS and SAMX and counts as 2 matches.
+    :param search_string: String to search for matches
+    :return: num_matches is the number of matches
+    """
+    xmas_regex = r'XMAS'
+    samx_regex = r'SAMX'
+
+    num_matches = 0
+
+    num_matches += len(re.findall(samx_regex, search_string))
+    num_matches += len(re.findall(xmas_regex, search_string))
+
+    return num_matches
